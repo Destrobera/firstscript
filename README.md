814bc978bf660a3614af72398fa08ff131c5b454
admin <belka4play@gmail.com>
2022-11-19 14:31:51 +0200

    commit message

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# firstscript
+# 
---
+++ data_types.py
@@ -0,0 +1,19 @@
+apple = 13
+print(apple)
+Alive = True
+print(Alive)
+Dead = False
+print(Dead)
+pineapple = 22.90
+tomato = 10
+peach = 25
+print("Ціна:", tomato)
+print("Вага:", peach)
+print("Frukt:", pineapple)
+a = 0b11
+print(a)
+b = 0o17
+print("число в восьмирічній системі кодування", b)
+name = "Makson"
+age = "18 years"
+print(name, age)
