d97c3294dc8c30d9a4ab43ccafd5c6bab252707e
admin <belka4play@gmail.com>
2022-11-25 12:56:52 +0200

    commit message

---
+++ boll_type.py
@@ -0,0 +1,17 @@
+comparison_result: bool = 3 != 5
+print(comparison_result)
+example_result: bool = 5 == 5
+example1: bool = 5 <= 5
+example2: bool = 5 >= 5
+print(example_result, example1, example2)
+example3: bool = True or True or False
+example4: bool = True and True or False
+example5: bool = True or True and False
+example6: bool = True or not True or False
+example7: bool = True and True and not False
+print(example3, example4, example5, example6, example7)
+example8: bool = None == 7
+example9: bool = " " == 10 - 1
+example10: bool = True or False == print(example_result)
+example11: bool = type(None) == id(None)
+print(example8, example9, example10, example11)
---
+++ rows_and_numbers.py
@@ -0,0 +1,17 @@
+username = input("Please tell me your name ")
+result = f"Hello {username}, nice to meet you!"
+print(result)
+score = float(input("say your number"))
+result1 = int(score)
+result2 = int(score)
+print(float(result1))
+print(result2)
+result3 = score ** 4
+print(result3)
+result4 = int(score) ** 0.5
+print(result4)
+division = float(input("на скільки частин треба поділити число"))
+division1 = score / division
+print("отримали результат", division1)
+division_remainder = score % division
+print("залишок від ділення", division_remainder)
